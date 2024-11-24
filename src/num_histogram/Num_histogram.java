ecc49aadca479b58fa072eda7525a37c3165b057
yousefalaa47 <86966633+yousefalaa47@users.noreply.github.com>
2024-11-24 18:10:50 +0200

    Add files via upload

---
+++ src/num_histogram/Num_histogram.java
@@ -0,0 +1,25 @@
+
+package num_histogram;
+import java .util.* ;
+public class Num_histogram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine() ;
+        int x = input.nextInt();
+        for (int i = 1; i <= x; i++) {
+            int y = input.nextInt();
+            if (i != 1) {
+                System.out.println();
+            }
+            for (int j = 1; j <= y; j++) {
+                System.out.print(s);
+                
+            }
+            
+            
+        }
+        
+    
+    }
+    
+}
